79fed82271214a5ed05985ac0c11fc8346cb690c
travelxcoder3 <coder3.travelx@gmail.online>
2026-07-21 09:49:29 +0300

    مشاكل عديده مه حل كروت

--- src/Models/SaasCompanyOtherinfo.php
+++ src/Models/SaasCompanyOtherinfo.php
@@ -14,12 +14,14 @@ class SaasCompanyOtherinfo extends Model
         'subscription_starts_at', 'subscription_ends_at',
         'allowed_users', 'timezone', 'default_locale', 'datetime_format',
         'default_annual_leave_days', 'excluded_leave_contract_types',
+        'general_manager_employee_id',
     ];
 
     protected $casts = [
         'subscription_starts_at' => 'date',
         'subscription_ends_at' => 'date',
         'allowed_users' => 'integer',
+        'general_manager_employee_id' => 'integer',
         'excluded_leave_contract_types' => 'array',
     ];
 
